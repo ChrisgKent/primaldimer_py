d5622b97d7e9f61cbb0dc84890d896e61a007a06
ChrisKent <chrisgkent@gmail.com>
2023-06-23 16:27:43 +0100

    added acces to calc_at_offset

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-use primaldimer_rs::{do_pools_interact, do_seqs_interact};
+use primaldimer_rs::{calc_at_offset, do_pools_interact, do_seqs_interact, encode_base};
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn calc_at_offset_py(seq1: &str, seq2: &str, offset: i32) -> f64 {
+    //Provide strings in 5'-3'
+    // This will return the score for this offset
+    let seq1 = encode_base(seq1);
+    let mut seq2 = encode_base(seq2);
+    seq2.reverse();
+
+    match calc_at_offset(&seq1, &seq2, offset) {
+        Some(score) => return score,
+        None => return 100.,
+    };
+}
 #[pyfunction]
 fn do_seqs_interact_py(seq1: &str, seq2: &str, t: f64) -> bool {
     return do_seqs_interact(seq1, seq2, t);
@@ -16,5 +28,6 @@ fn do_pools_interact_py(pool1: Vec<&str>, pool2: Vec<&str>, t: f64) -> bool {
 fn primaldimer_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(do_pools_interact_py, m)?)?;
     m.add_function(wrap_pyfunction!(do_seqs_interact_py, m)?)?;
+    m.add_function(wrap_pyfunction!(calc_at_offset_py, m)?)?;
     Ok(())
 }
