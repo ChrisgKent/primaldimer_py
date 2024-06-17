be25aac5ceb3840bc680edbc13f851e1cf46a5db
ChrisKent <chrisgkent@gmail.com>
2024-06-17 12:44:53 +0100

    Refactored primaldimer_rs as a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use primaldimer_rs::{calc_at_offset, do_pools_interact, do_seqs_interact, encode_base};
+mod primaldimer;
+
 use pyo3::prelude::*;
 
 use debruijn::dna_string::*;
@@ -50,22 +51,22 @@ impl Kmer {
 fn calc_at_offset_py(seq1: &str, seq2: &str, offset: i32) -> f64 {
     //Provide strings in 5'-3'
     // This will return the score for this offset
-    let seq1 = encode_base(seq1);
-    let mut seq2 = encode_base(seq2);
+    let seq1 = primaldimer::encode_base(seq1);
+    let mut seq2 = primaldimer::encode_base(seq2);
     seq2.reverse();
 
-    match calc_at_offset(&seq1, &seq2, offset) {
+    match primaldimer::calc_at_offset(&seq1, &seq2, offset) {
         Some(score) => return score,
         None => return 100.,
     };
 }
 #[pyfunction]
 fn do_seqs_interact_py(seq1: &str, seq2: &str, t: f64) -> bool {
-    return do_seqs_interact(seq1, seq2, t);
+    return primaldimer::do_seqs_interact(seq1, seq2, t);
 }
 #[pyfunction]
 fn do_pools_interact_py(pool1: Vec<&str>, pool2: Vec<&str>, t: f64) -> bool {
-    return do_pools_interact(pool1, pool2, t);
+    return primaldimer::do_pools_interact(pool1, pool2, t);
 }
 
 /// A Python module implemented in Rust.
